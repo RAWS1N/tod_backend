a59b91d90d0454e2a130f75e91484c4bdac62a8b
RAWSIN <diveshkumarsingariya@gmail.com>
2023-04-06 09:30:59 +0530

    configuration changed

--- data/database.js
+++ data/database.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.COLLECTION}`, {
+    const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.COLLECTION}`, {
       dbName: process.env.DATABASE,
     });
-    console.log("database is connected");
+    console.log(`database is connected with ${connection.connection.host}`);
   } catch (e) {
     console.log(e.message);
   }
