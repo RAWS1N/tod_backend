4d8bcd1db69e8ab4653039b1d18b8bc405ee1ae8
RAWSIN <diveshkumarsingariya@gmail.com>
2023-04-15 08:55:36 +0530

    cors policy changed

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ config({
 
 
 const app = express()
-app.use(cors())
+// app.use(cors())
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json())
 app.use(cookieParser())
 app.get("/",(req,res) => {
